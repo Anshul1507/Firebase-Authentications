35d5caf2da9ada8b9f4f6443430a34f961ee99bf
Anshul1507 <ag1507anshul@gmail.com>
2021-04-09 02:41:31 +0530

    Added: View Binding Support

--- app/src/main/java/tech/anshul1507/firebase_authentications/MainActivity.kt
+++ app/src/main/java/tech/anshul1507/firebase_authentications/MainActivity.kt
@@ -1,11 +1,32 @@
 package tech.anshul1507.firebase_authentications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tech.anshul1507.firebase_authentications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        onClickListeners()
+    }
+
+    private fun onClickListeners() {
+
+        /**
+         *  E-Mail Sign in Button Click Listener
+         *  => Opens up PasswordActivity.kt on Click
+         * */
+        viewBinding.emailSignIn.setOnClickListener {
+            startActivity(Intent(this, PasswordActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/tech/anshul1507/firebase_authentications/PasswordActivity.kt
+++ app/src/main/java/tech/anshul1507/firebase_authentications/PasswordActivity.kt
@@ -2,10 +2,17 @@ package tech.anshul1507.firebase_authentications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tech.anshul1507.firebase_authentications.databinding.ActivityMainBinding
+import tech.anshul1507.firebase_authentications.databinding.ActivityPasswordBinding
 
 class PasswordActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityPasswordBinding by lazy {
+        ActivityPasswordBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_password)
+        setContentView(viewBinding.root)
     }
 }
